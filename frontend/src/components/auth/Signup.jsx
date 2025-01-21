ab2e1be2407f627dcb366df76eae289c99aaa78c
Abhishek <choudharyabhishekk@gmail.com>
2025-01-21 15:41:30 -0500

    fix: background color fix

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -117,7 +117,7 @@ const Signup = () => {
 
   return (
     <div>
-      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="min-h-screen flex items-center justify-center  py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8 bg-white p-10 rounded-xl shadow-sm">
           <div>
             <h1 className="text-3xl font-semibold text-center text-gray-900">
--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 mt-4 py-10">
+    <footer className="bg-white mt-4 py-10">
       <div className="container mx-auto px-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
           {/* Quick Links */}
